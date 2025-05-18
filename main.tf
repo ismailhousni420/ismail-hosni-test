aa209e622b1c66732a8838cd83b26cfb9a2203dd
ismailhousni420 <ismailhousni420@gmail.com>
2025-05-18 23:08:27 +0100

    first comit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+
+provider "openstack" {
+  cloud  = "osp_admin" # cloud defined in cloud.yml file
+}
+
+resource "openstack_compute_instance_v2" "server" {
+  name            = "Debian10"  #Instance name
+  image_id        = data.openstack_images_image_v2.image.id
+  flavor_id       = data.openstack_compute_flavor_v2.flavor.id
+  key_pair        = var.keypair
+  security_groups = var.security_groups
+
+  network {
+    name = var.network
+  }
+}
