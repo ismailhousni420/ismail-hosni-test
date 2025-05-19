3d48401b58a1181aa3b8cf78540a04833728f53f
ismailhousni420 <ismailhousni420@gmail.com>
2025-05-19 19:01:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 
 
 provider "openstack" {
-  cloud  = "osp_admin" # cloud defined in cloud.yml file
+  cloud  = "osp_admin" 
 }
 
 resource "openstack_compute_instance_v2" "server" {
-  name            = "Debian10"  #Instance name
+  name            = "Debian10"  
   image_id        = data.openstack_images_image_v2.image.id
   flavor_id       = data.openstack_compute_flavor_v2.flavor.id
   key_pair        = var.keypair
